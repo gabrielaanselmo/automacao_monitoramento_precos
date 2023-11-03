07eaca933b4b505947e320d0b60995cfea92905d
Maria Gabriela <gabrielaanselmao@gmail.com>
2023-11-03 20:47:38 -0300

    [feat] workflows

--- automacao/script.py
+++ automacao/script.py
@@ -2,7 +2,6 @@ import os
 import time
 import requests
 import json
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
